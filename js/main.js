adc92b2e74e7b62037bf42c0aab24a2d7688ef67
GfsTee <georg.treitz@gmail.com>
2019-10-27 17:01:23 +0100

    Participants added / unchecked

--- js/main.js
+++ js/main.js
@@ -2,36 +2,32 @@
 var result = document.getElementById("result");
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
-    
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
-    
-        // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-    
-        // And swap it with the current element.
+
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
     
     return array;
-    }
+}
 var res1 = document.getElementById("result1");
+var res2 = document.getElementById("result2");
+var res3 = document.getElementById("result3");
+var res4 = document.getElementById("result4");
+var res5 = document.getElementById("result5");
 
 
 function check() {
     var checkboxes = document.querySelectorAll('input[name="name"]:checked'), values = [];
     Array.prototype.forEach.call(checkboxes, function(el) {
         values.push(el.value);
-    });
+    }); 
     shuffle(values);
-    console.log(values);
     var group = document.getElementById("groups").value;
-    console.log("Groups:" + group);
     var parGroup = Math.floor(values.length/group);
-    console.log("Teilnehmer/Gruppe:" + parGroup);
     result.innerHTML = "";
     for (let j = 1; j<=group; j++) {
         for (let k = 1; k<=parGroup; k++) {
